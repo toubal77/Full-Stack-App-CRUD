ca12568ac51c4f672e9b00c7047c917157e41281
Elias Larbi <eliaslarbi755@gmail.com>
2024-12-25 16:50:00 +0100

    feat: category list, paginator, error messages & empty list

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { ListCategoriesComponent } from './list-categories/list-categories.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,16 @@ import { ListCategoriesComponent } from './list-categories/list-categories.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatSortModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    AppRoutingModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Client/src/app/list-categories/list-categories.component.ts
+++ Client/src/app/list-categories/list-categories.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-list-categories',
   templateUrl: './list-categories.component.html',
   styleUrls: ['./list-categories.component.scss']
 })
-export class ListCategoriesComponent {
+export class ListCategoriesComponent implements OnInit {
+  displayedColumns: string[] = ['Nom', 'Parent', 'Date_Creation', 'Enfant', 'Racine', 'actions'];
+  dataSource = new MatTableDataSource<any>([]);
+  msgError: any;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit(): void {
+    this.loadData();
+    //this.dataSource.data =[]; // pour affiche la liste vide
+    //this.msgError = "Message Erreur"; // pour affiche un message d'erreur
+    this.dataSource.paginator = this.paginator;
+    console.log(this.dataSource); 
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  loadData(): void {
+    this.dataSource.data = [
+      { Nom: "Categorie 1", Parent: " - ", Date_Creation: '06-10-2024', Enfant: 'Categorie 2', Racine: 'Oui' },
+      { Nom: "Categorie 2", Parent: "Categorie 1", Date_Creation: '06-10-2024', Enfant: ' - ', Racine: 'Non' },
+      { Nom: "Categorie 3", Parent: " - ", Date_Creation: '06-10-2024', Enfant: ' - ', Racine: 'Oui' },
+      { Nom: "Categorie 1", Parent: " - ", Date_Creation: '06-10-2024', Enfant: 'Categorie 2', Racine: 'Oui' },
+      { Nom: "Categorie 2", Parent: "Categorie 1", Date_Creation: '06-10-2024', Enfant: ' - ', Racine: 'Non' },
+      { Nom: "Categorie 3", Parent: " - ", Date_Creation: '06-10-2024', Enfant: ' - ', Racine: 'Oui' },
+      { Nom: "Categorie 1", Parent: " - ", Date_Creation: '06-10-2024', Enfant: 'Categorie 2', Racine: 'Oui' },
+      { Nom: "Categorie 2", Parent: "Categorie 1", Date_Creation: '06-10-2024', Enfant: ' - ', Racine: 'Non' },
+      { Nom: "Categorie 3", Parent: " - ", Date_Creation: '06-10-2024', Enfant: ' - ', Racine: 'Oui' },
+      { Nom: "Categorie 1", Parent: " - ", Date_Creation: '06-10-2024', Enfant: 'Categorie 2', Racine: 'Oui' },
+      { Nom: "Categorie 2", Parent: "Categorie 1", Date_Creation: '06-10-2024', Enfant: ' - ', Racine: 'Non' },
+      { Nom: "Categorie 3", Parent: " - ", Date_Creation: '06-10-2024', Enfant: ' - ', Racine: 'Oui' },
+    ];
+   
+  }
+
+  editCategory(row: any[]): void {
+    console.log('Edit', row);
+  }
+
+  deleteCategory(row: any[]): void {
+    console.log('Delete', row);
+  }
 }
